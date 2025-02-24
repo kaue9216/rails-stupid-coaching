a2a0a8b280346a22cafdfee7b4ac00316e0daa62
kaue ogibowski <kaueogibowski@outlook.com>
2025-02-24 15:42:43 -0300

    Stupid Coaching

---
+++ app/helpers/quastions_helper.rb
@@ -0,0 +1,2 @@
+module QuastionsHelper
+end
---
+++ test/controllers/quastions_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class QuastionsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
